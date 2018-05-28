ce8d3a0d9891e84a706edd41331ae846f9eaa8e8
Unknown <qkfrlgy@naver.com>
2018-05-28 11:37:01 +0900

    javascript 1회차: 변수 boolean 예제

--- Javascript/day1/js/variable_number.js
+++ Javascript/day1/js/variable_number.js
@@ -7,3 +7,6 @@ console.log("number2" + number2);
 console.log("is finite number1 :" + isFinite(number1));
 console.log("is finite number2 :" + isFinite(number2));
 console.log("is finite number3 :" + isFinite(number3));
+
+var number4 = "10";
+console.log("is NaN number4: " + isNaN(number4)); //javascript는 var에 다 넣을 수 있기 떄문에, 숫자인지 아닌지를 판별하는게 필요함
