cca5125bd637d7c850ca62269845007786fed967
Unknown <qkfrlgy@naver.com>
2018-05-28 11:21:18 +0900

    javascript 1회차: number 예제

---
+++ Javascript/day1/js/variable_number.js
@@ -0,0 +1,9 @@
+var number1 = 1/0;
+var number2 = -1/0;
+var number3 = 10;
+
+console.log("number1" + number1);
+console.log("number2" + number2);
+console.log("is finite number1 :" + isFinite(number1));
+console.log("is finite number2 :" + isFinite(number2));
+console.log("is finite number3 :" + isFinite(number3));
