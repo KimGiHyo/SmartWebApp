305a77670a4be4e00b2275a34de369b89800dc3b
Unknown <qkfrlgy@naver.com>
2018-05-28 11:01:19 +0900

    javascript 1회차: 변수string 예제

---
+++ Javascript/day1/js/variable_string.js
@@ -0,0 +1,21 @@
+var str1 = "My name is";
+var str2 = 'Java script';
+
+var str3 = String("My name is");
+var str4 = new String("Java script");
+var str5 = str2.valueOf();
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str5);
+
+
+console.log("A" > "B");
+console.log("A" > "AB");
+console.log("A" > "a");
+console.log("3" > "10");
+console.log(3 > "10");
+console.log(3 > 10);
+
