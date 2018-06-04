dfb89641e191040e476b496562c80a5896207109
Unknown <qkfrlgy@naver.com>
2018-06-04 12:31:21 +0900

    javascript 3회차 : Math 객체 예제

---
+++ Javascript/day3/js/math.js
@@ -0,0 +1,10 @@
+console.log("Math.PI" + Math.PI);
+console.log("Math.E" + Math.E);
+console.log("Math.pow(2,3) : " + Math.pow(2,3)) // 지수함수 2의3세제곱
+console.log("Math.sqrt(4) : " + Math.sqrt(4)); // 제곱근
+console.log("Math.ceil(1.3) : " + Math.ceil(1.3)); // 올림
+console.log("Math.round(1.3) : " + Math.round(1.3)); // 반올림
+console.log("Math.floor(1.9) :  " + Math.floor(1.9)); // 내림
+console.log("Math.min(1,4) : " + Math.min(1,4)); // 두 수 중에 작은거 리턴
+console.log("Math.max(1,4) : " + Math.max(1,4)); // 두 수 중에 작은거 리턴
+console.log("Math.random() : " + Math.random());
